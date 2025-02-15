85db5da8144d89ad92c340a65b486332f08549b9
bshongwe <dru.da.beat.hustle@gmail.com>
2025-02-16 01:06:21 +0200

    Links replacement

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -29,12 +29,12 @@ const social = [
   {
     name: "GitHub",
     icon: "github",
-    link: "https://github.com/once-ui-system/nextjs-starter",
+    link: "https://github.com/bshongwe",
   },
   {
     name: "LinkedIn",
     icon: "linkedin",
-    link: "https://www.linkedin.com/company/once-ui/",
+    link: "https://www.linkedin.com/in/ernest-shongwe/",
   },
   {
     name: "X",
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -28,10 +28,10 @@ export const Footer = () => {
           <Text paddingX="4">{person.name}</Text>
           <Text onBackground="neutral-weak">
             {/* Usage of this template requires attribution. Please don't remove the link to Once UI. */}
-            / Build your portfolio with{" "}
+            / Connect with me{" "}
             <SmartLink
               style={{ marginLeft: "-0.125rem" }}
-              href="https://once-ui.com/templates/magic-portfolio"
+              href="https://ernestshong.me"
             >
               Once UI
             </SmartLink>
