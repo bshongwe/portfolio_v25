3f5ae932f870458248fea1b5c18399289bc2dfda
bshongwe <dru.da.beat.hustle@gmail.com>
2025-02-15 20:00:02 +0200

    Revamp: new

--- config/base-url.config.ts
+++
@@ -1,5 +0,0 @@
-export const BASE_API_URI =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:5000/api/v1"
-    : process.env.NEXT_PUBLIC_BASE_API_URI;
-export const BASE_NAME = process.env.NEXT_PUBLIC_BASE_NAME || "Next.js";
--- next.config.mjs
+++ next.config.mjs
@@ -1,47 +1,13 @@
+import mdx from "@next/mdx";
+
+const withMDX = mdx({
+  extension: /\.mdx?$/,
+  options: {},
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  cacheMaxMemorySize: 1024 * 1024 * 100,
-  compress: true,
-  images: {
-    unoptimized: true,
-    minimumCacheTTL: 60 * 60 * 24,
-    dangerouslyAllowSVG: true,
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-      },
-      {
-        protocol: "https",
-        hostname: "avatars.githubusercontent.com",
-      },
-      {
-        protocol: "https",
-        hostname: "aws.amazon.com",
-      },
-      {
-        protocol: "https",
-        hostname: "assets.aceternity.com",
-      },
-
-      {
-        protocol: "https",
-        hostname: "avatar.vercel.sh",
-      },
-      {
-        protocol: "https",
-        hostname: "s3.amazonaws.com",
-      },
-    ],
-  },
+  pageExtensions: ["ts", "tsx", "md", "mdx"],
 };
 
-export default nextConfig;
+export default withMDX(nextConfig);
