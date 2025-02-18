d48f6f1ac47a971eb2e9d734d9ca4ddf52d84ca4
bshongwe <dru.da.beat.hustle@gmail.com>
2025-02-18 11:49:14 +0200

    RE: locking Nestec blog access

--- src/app/resources/config.js
+++ src/app/resources/config.js
@@ -11,7 +11,7 @@ const routes = {
 // Enable password protection on selected routes
 // Set password in the .env file, refer to .env.example
 const protectedRoutes = {
-  "/work/projects/customer-churn-prediction-mlflow-dvc-e2e-pipeline.mdx": true,
+  "/work/customer-churn-prediction-mlflow-dvc-e2e-pipeline.mdx": true,
 };
 
 const style = {
