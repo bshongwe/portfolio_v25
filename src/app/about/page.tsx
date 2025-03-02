3c766d560e5136ebe3839e39a239a5d900e178f7
bshongwe <dru.da.beat.hustle@gmail.com>
2025-03-02 17:12:08 +0200

    Insert dates display

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -285,6 +285,9 @@ export default function About() {
                     <Text variant="heading-default-xs" onBackground="neutral-weak">
                       {institution.description}
                     </Text>
+                    <Text variant="heading-default-xs" onBackground="neutral-weak">
+                        {institution.timeframe}
+                    </Text>
                   </Column>
                 ))}
               </Column>
